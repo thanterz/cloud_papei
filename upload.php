cb47fcc783bc324c4b9b464a4ba8f7b42befae03
Than Terz <thanterz@gmail.com>
2015-11-18 18:12:42 +0200

    access key update

--- upload.php
+++ upload.php
@@ -7,7 +7,7 @@
   <body> 
     <form action="https://mpsp14087.s3.amazonaws.com/" method="post" enctype="multipart/form-data">
       <input type="hidden" name="key" value="uploads/${filename}">
-      <input type="hidden" name="AWSAccessKeyId" value=" AKIAJ544BGTU6SCLOAWA "> 
+      <input type="hidden" name="AWSAccessKeyId" value="AKIAJ544BGTU6SCLOAWA"> 
       <input type="hidden" name="acl" value="private"> 
       <input type="hidden" name="success_action_redirect" value="http://localhost/">
       <input type="hidden" name="policy" value="eyJleHBpcmF0aW9uIjogIjIwMTktMDEtMDFUMDA6MDA6MDBaIiwiY29uZGl0aW9ucyI6IFsgeyJidWNrZXQiOiAiczMtYnVja2V0In0sIFsic3RhcnRzLXdpdGgiLCAiJGtleSIsICJ1cGxvYWRzLyJdLHsiYWNsIjogInByaXZhdGUifSx7InN1Y2Nlc3NfYWN0aW9uX3JlZGlyZWN0IjogImh0dHA6Ly9sb2NhbGhvc3QvIn0sWyJzdGFydHMtd2l0aCIsICIkQ29udGVudC1UeXBlIiwgIiJdLFsiY29udGVudC1sZW5ndGgtcmFuZ2UiLCAwLCAxMDQ4NTc2XV19">
