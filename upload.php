30ce1280078770a1464b9d1ee0b5249950321ebf
Than Terz <thanterz@gmail.com>
2015-11-23 18:54:52 +0200

    ;lkasjdfd

--- upload.php
+++ upload.php
@@ -7,12 +7,12 @@
   <body> 
     <form action="http://mpsp14087.s3.amazonaws.com/" method="post" enctype="multipart/form-data">
       <input type="hidden" name="key" value="uploads/${filename}">
-      <input type="hidden" name="AWSAccessKeyId" value="AKIAJ544BGTU6SCLOAWA"> 
-      <input type="hidden" name="acl" value="private"> 
+      <input type="hidden" name="AWSAccessKeyId" value="AKIAJQX5LLONIU3JL55Q"> 
+      <input type="hidden" name="acl" value="public"> 
       <input type="hidden" name="success_action_redirect" value="http://localhost/">
       <input type="hidden" name="policy" value="eyJleHBpcmF0aW9uIjogIjIwMTktMDEtMDFUMDA6MDA6MDBaIiwiY29uZGl0aW9ucyI6IFsgeyJidWNrZXQiOiAiczMtYnVja2V0In0sIFsic3RhcnRzLXdpdGgiLCAiJGtleSIsICJ1cGxvYWRzLyJdLHsiYWNsIjogInByaXZhdGUifSx7InN1Y2Nlc3NfYWN0aW9uX3JlZGlyZWN0IjogImh0dHA6Ly9sb2NhbGhvc3QvIn0sWyJzdGFydHMtd2l0aCIsICIkQ29udGVudC1UeXBlIiwgIiJdLFsiY29udGVudC1sZW5ndGgtcmFuZ2UiLCAwLCAxMDQ4NTc2XV19">
-      <input type="hidden" name="signature" value="bmYbMGLDhsJsVGLatkl4sH1Vb/4=">
-      <input type="hidden" name="Content-Type" value="image/jpeg">
+      <input type="hidden" name="signature" value="Qe3nKct0ZiZi24cdpgJZb2FdfOo=">
+      <input type="hidden" name="Content-Type" value="image/jpg">
       <!-- Include any additional input fields here -->
 
       File to upload to S3: 
