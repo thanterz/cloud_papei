30a0d1b20bfe62384d27adf5df9507bcdd0a73fa
Than Terz <thanterz@gmail.com>
2015-11-23 18:31:38 +0200

    index

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+<form action="http://s3.amazonaws.com/mpsp14087" method="post" enctype="multipart/form-data">
+<input type="text" name="key" value="testfile.txt" />
+<input type="text" name="acl" value="public-read" />
+<input type="text" name="content-type" value="text/plain" />
+<input type="hidden" name="AWSAccessKeyId" value="" />
+<input type="hidden" name="policy" value="ewogICJleHBpcmF0aW9uIjogIjIwMDktMDEtMDFUMTI6MDA6MDAuMDAwWiIsCiAgImNvbmRpdGlvbnMiOiBbCiAgICB7ImJ1Y2tldCI6ICI8YnVja2V0bmFtZT4iIH0sCiAgICB7ImFjbCI6ICJwdWJsaWMtcmVhZCIgfSwKICAgIFsiZXEiLCAiJGtleSIsICJ0ZXN0ZmlsZS50eHQiXSwKICAgIFsic3RhcnRzLXdpdGgiLCAiJENvbnRlbnQtVHlwZSIsICJ0ZXh0LyJdLAogIF0KfQo=" />
+<input type="hidden" name="signature" value="atTbrxKXXML9JsNVyOKI7FG0nCI=" />
+<input name="file" type="file" />
+<input name="submit" value="Upload" type="submit" />
+</form>
+</body>
+</html>
