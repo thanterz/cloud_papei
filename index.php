3416d1f764a07201e8bfeaa9f9fc9db8a40d85e8
Than Terz <thanterz@gmail.com>
2015-11-23 19:57:02 +0200

    index final

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 </head>
 <body>
-<form action="http://s3.amazonaws.com/mpsp14087" method="post" enctype="multipart/form-data">
-<input type="text" name="key" value="testfile.txt" />
+<form action="http://mpsp14087.s3.amazonaws.com/" method="post" enctype="multipart/form-data">
+<input type="text" name="key" value="uploads/${filename}" />
 <input type="text" name="acl" value="public-read" />
 <input type="text" name="content-type" value="text/plain" />
-<input type="hidden" name="AWSAccessKeyId" value="" />
-<input type="hidden" name="policy" value="ewogICJleHBpcmF0aW9uIjogIjIwMDktMDEtMDFUMTI6MDA6MDAuMDAwWiIsCiAgImNvbmRpdGlvbnMiOiBbCiAgICB7ImJ1Y2tldCI6ICI8YnVja2V0bmFtZT4iIH0sCiAgICB7ImFjbCI6ICJwdWJsaWMtcmVhZCIgfSwKICAgIFsiZXEiLCAiJGtleSIsICJ0ZXN0ZmlsZS50eHQiXSwKICAgIFsic3RhcnRzLXdpdGgiLCAiJENvbnRlbnQtVHlwZSIsICJ0ZXh0LyJdLAogIF0KfQo=" />
-<input type="hidden" name="signature" value="atTbrxKXXML9JsNVyOKI7FG0nCI=" />
+<input type="hidden" name="AWSAccessKeyId" value="AKIAJQX5LLONIU3JL55Q" />
+<input type="hidden" name="policy" value="ewogICJleHBpcmF0aW9uIjogIjIwMTktMDEtMDFUMTI6MDA6MDAuMDAwWiIsCiAgImNvbmRpdGlvbnMiOiBbCiAgICB7ImJ1Y2tldCI6ICJtcHNwMTQwODciIH0sCiAgICB7ImFjbCI6ICJwdWJsaWMtcmVhZCIgfSwKICAgIFsic3RhcnRzLXdpdGgiLCAiJGtleSIsICJ1cGxvYWRzLyJdLAogICAgWyJzdGFydHMtd2l0aCIsICIkQ29udGVudC1UeXBlIiwgIiJdLAogIF0KfQo=" />
+<input type="hidden" name="signature" value="zrqac1BcE5hbdYO9mGacJM4P5pA=" />
 <input name="file" type="file" />
 <input name="submit" value="Upload" type="submit" />
 </form>
